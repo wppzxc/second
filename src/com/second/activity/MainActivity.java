685a2baf2187ed67fae7d1da5c47f0bb906e4acd
wpp <bachinanfei@qq.com>
2016-04-24 19:36:45 +0800

    main_activity_viewpager

--- src/com/second/activity/MainActivity.java
+++ src/com/second/activity/MainActivity.java
@@ -1,8 +1,16 @@
 package com.second.activity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.example.second.R;
+
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.ViewPager;
+import android.support.v4.view.ViewPager.OnPageChangeListener;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -19,22 +27,16 @@ public class MainActivity extends BaseActivity {
 	ImageView second_iv_shop;
 	ImageView second_iv_friend;
 	ImageView second_iv_self;
+	ViewPager main_vp;
 
 	private int chooseIndex = -1;
 	private long firstTime = 0;
 
 	private FragmentTransaction ft;
-
-	// 控件绑定
-	private void initViews() {
-		second_llyt_shop = (LinearLayout) findViewById(R.id.second_llyt_shop);
-		second_llyt_friend = (LinearLayout) findViewById(R.id.second_llyt_friend);
-		second_llyt_self = (LinearLayout) findViewById(R.id.second_llyt_self);
-		second_iv_shop = (ImageView) findViewById(R.id.second_iv_shop);
-		second_iv_friend = (ImageView) findViewById(R.id.second_iv_friend);
-		second_iv_self = (ImageView) findViewById(R.id.second_iv_self);
-
-	}
+	private ShopFragment shopFragment;
+	private FriendFragment friendFragment;
+	private SelfFragment selfFragment;
+	private List<Fragment> fragmentsList = new ArrayList<Fragment>();
 
 	@Override
 	protected void onCreate(Bundle saveInstanceState) {
@@ -44,6 +46,17 @@ public class MainActivity extends BaseActivity {
 		if (saveInstanceState == null) {
 			onClick(second_llyt_shop);
 		}
+	}
+
+	// 控件绑定
+	private void initViews() {
+		second_llyt_shop = (LinearLayout) findViewById(R.id.second_llyt_shop);
+		second_llyt_friend = (LinearLayout) findViewById(R.id.second_llyt_friend);
+		second_llyt_self = (LinearLayout) findViewById(R.id.second_llyt_self);
+		second_iv_shop = (ImageView) findViewById(R.id.second_iv_shop);
+		second_iv_friend = (ImageView) findViewById(R.id.second_iv_friend);
+		second_iv_self = (ImageView) findViewById(R.id.second_iv_self);
+		main_vp = (ViewPager) findViewById(R.id.main_vp);
 
 	}
 
@@ -55,6 +68,7 @@ public class MainActivity extends BaseActivity {
 		second_llyt_self.setOnClickListener(this);
 	}
 
+	// 控件点击事件
 	@Override
 	public void onClick(View v) {
 
@@ -101,18 +115,51 @@ public class MainActivity extends BaseActivity {
 
 	}
 
+	public class DefineOnPageChangeListener implements OnPageChangeListener {
+
+		@Override
+		public void onPageScrollStateChanged(int arg0) {
+
+		}
+
+		@Override
+		public void onPageScrolled(int arg0, float arg1, int arg2) {
+
+		}
+
+		@Override
+		public void onPageSelected(int arg0) {
+			switch (arg0) {
+			case 0:
+				tabBgChange(0);
+				break;
+			case 1:
+				tabBgChange(1);
+				break;
+			case 2:
+				tabBgChange(2);
+				break;
+
+			}
+
+		}
+
+	}
+
 	private void tabBgChange(int index) {
 		switch (index) {
 		case 0:
 			second_iv_shop.setSelected(true);
 			second_iv_friend.setSelected(false);
 			second_iv_self.setSelected(false);
+			main_vp.setCurrentItem(0);
 
 			break;
 		case 1:
 			second_iv_shop.setSelected(false);
 			second_iv_friend.setSelected(true);
 			second_iv_self.setSelected(false);
+			main_vp.setCurrentItem(1);
 
 			break;
 
@@ -120,6 +167,7 @@ public class MainActivity extends BaseActivity {
 			second_iv_shop.setSelected(false);
 			second_iv_friend.setSelected(false);
 			second_iv_self.setSelected(true);
+			main_vp.setCurrentItem(2);
 
 			break;
 
@@ -132,8 +180,31 @@ public class MainActivity extends BaseActivity {
 		return R.layout.activity_main;
 	}
 
+	@SuppressWarnings("deprecation")
 	@Override
 	protected void initParams() {
+		shopFragment = new ShopFragment();
+		friendFragment = new FriendFragment();
+		selfFragment = new SelfFragment();
+
+		fragmentsList.add(shopFragment);
+		fragmentsList.add(friendFragment);
+		fragmentsList.add(selfFragment);
+
+		main_vp.setAdapter(new FragmentStatePagerAdapter(getSupportFragmentManager()) {
+			@Override
+			public int getCount() {
+				return fragmentsList.size();
+			}
+
+			@Override
+			public Fragment getItem(int arg0) {
+				return fragmentsList.get(arg0);
+			}
+		});
+
+		main_vp.setCurrentItem(0);
+		main_vp.setOnPageChangeListener(new DefineOnPageChangeListener());
 	}
 
 	@Override
